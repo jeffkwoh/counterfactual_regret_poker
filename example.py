af8e302ffffb3d6b36a9cdee062dee89ad62e9de
Teo Jun Jie <teojunjie@gmail.com>
2019-04-15 19:57:26 +0800

    Renamed cfr agent to group19player to accomdate upload requirements for cs3243 website

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 from pypokerengine.api.game import setup_config, start_poker
 from randomplayer import RandomPlayer
 from raise_player import RaisedPlayer
-from CFRAgent import CFRAgent
+from Group19Player import Group19Player
 
 #TODO:config the config as our wish
 config = setup_config(max_round=1000, initial_stack=10000, small_blind_amount=10)
 
 config.register_player(name="AlwaysRaisedPlayer", algorithm=RaisedPlayer())
-config.register_player(name="CFRAgent", algorithm=CFRAgent())
+config.register_player(name="Group19Player", algorithm=Group19Player())
 
 game_result = start_poker(config, verbose=0)
