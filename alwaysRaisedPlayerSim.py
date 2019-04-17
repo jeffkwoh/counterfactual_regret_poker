8752092a9b5b8bc4d29b8ad3c69ef0f97491fb56
Teo Jun Jie <teojunjie@gmail.com>
2019-04-17 17:40:13 +0800

    Modified alwaysraisedplayersim.py becoz i was doing it wrongly

--- alwaysRaisedPlayerSim.py
+++ alwaysRaisedPlayerSim.py
@@ -7,55 +7,64 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) < 1:
-        print("Usage {strategyFileNumber : {0-9}")
+        print("Usage {strategyFile}")
         sys.exit(1)
-    num_rounds = 500
-    num_games = [2000,3000,4000,5000,6000,7000,8000,9000,10000]
 
-    text_files = ['resultsStrategy10000e_2000games.txt','resultsStrategy10000e_3000games.txt',
-                  'resultsStrategy10000e_4000games.txt','resultsStrategy10000e_5000games.txt',
-                  'resultsStrategy10000e_6000games.txt','resultsStrategy10000e_7000games.txt',
-                  'resultsStrategy10000e_8000games.txt','resultsStrategy10000e_9000games.txt',
-                  'resultsStrategy10000e_10000games.txt']
-
-    strategyFileNumber = int(sys.argv[1], 10)
+    strategyFileResults = sys.argv[1]
     # num_games = int(sys.argv[2], 10)
     # output_text_file = sys.argv[3]
 
-    current_text_file = text_files[strategyFileNumber]
-    current_games = num_games[strategyFileNumber]
-    results = open(current_text_file,"w")
-    for i in range(current_games) :
+    results = open(strategyFileResults,"w")
+    
+    game_data = {}
+    
+    for i in range(1000) :
         #TODO:config the config as our wish
-        config = setup_config(max_round=num_rounds, initial_stack=10000, small_blind_amount=20)
+        config = setup_config(max_round=500, initial_stack=10000, small_blind_amount=20)
 
         config.register_player(name="AlwaysRaisedPlayer", algorithm=RaisedPlayer())
         config.register_player(name="Group19Player", algorithm=Group19Player())
         game_result = start_poker(config, verbose=0)
 
-        game_data = {}
 
-        for player in game_result['players'] :
-            player_name = player['name']
-            player_stack = player['stack']
-            player_num_wins = game_result['result'][player_name]
-            game_data[player_name] = {
-                'num_wins' : player_num_wins,
-                'num_loses' : game_result['num_rounds'] - player_num_wins,
-                'game_stack' : player_stack
-            }
+        player1 = game_result['players'][0]
+        player1_name = player1['name']
+        player1_stack = player1['stack']
+        player2 = game_result['players'][1]
+        player2_name = player2['name']
+        player2_stack = player2['stack']
+        
+        if  player1_stack > player2_stack :
+            try :
+                if game_data[player1_name] :
+                    game_data[player1_name] += 1
+            except KeyError :
+                game_data[player1_name] = 1
+    
+        elif player2_stack > player1_stack :
+            try :
+                if game_data[player2_name] :
+                    game_data[player2_name] += 1
+            except KeyError :
+                game_data[player2_name] = 1
+
+        else : 
+            try :
+                if game_data[player1_name] :
+                    game_data[player1_name] += 0.5
+            except KeyError :
+                game_data[player1_name] = 0.5
 
             try :
-                if game_data['winner'] :
-                    if game_data[player_name]['game_stack'] > game_data[game_data['winner']]['game_stack'] :
-                        game_data['winner'] = player_name
-
+                if game_data[player2_name] :
+                    game_data[player2_name] += 0.5
             except KeyError :
-                game_data['winner'] = player_name
+                game_data[player2_name] = 0.5
+                
+        print(i)        
 
-        print(i)
-        results.write(json.dumps(game_data))
-        results.write("\n")
+    results.write(json.dumps(game_data))
+    results.write("\n")
     
 
 
