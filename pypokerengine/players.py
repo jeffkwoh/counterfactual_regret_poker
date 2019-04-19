05babbb149e0ff0a228c5f832006b6b96bc86cc8
dlworldpeace <xdl_line@hotmail.com>
2019-04-19 13:51:30 +0800

    Update according to changes from CS3243 main repo.

--- pypokerengine/players.py
+++ pypokerengine/players.py
@@ -49,8 +49,12 @@ class BasePokerPlayer(object):
     for va in valid_actions:
       if action == va['action'] :
         return action
-    raise ValueError('Invalid Action Called')
-
+    # print('[BUGFIX-TA] : Action Called %s, valid actions : '%action)
+    # print(valid_actions)
+    # time.sleep(10)
+    return 'fold'
+    #raise ValueError('Invalid Action Called')
+    
   def receive_notification(self, message):
     """Called from Dealer when notification received from RoundManager"""
     msg_type = message["message_type"]
